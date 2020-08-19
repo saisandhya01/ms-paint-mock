e74264161b45b937d559a16ae0482eaa6ce005ea
Sai <saisandhya187@gmail.com>
2020-08-19 17:13:33 +0530

    Add the shape 'rectangle';

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth / 2;
 canvas.height = window.innerHeight;
+
 let eraseButton = document.getElementById("erase-button");
+let colorInput = document.getElementById("color");
+let shapeButton = document.getElementById("rectangle");
+
+let shape = false;
 let Drawing = false;
 let x = 0;
 let y = 0;
 let color = "black";
+
 eraseButton.onclick = () => {
   color = "white";
+};
+colorInput.onchange = () => {
+  color = colorInput.value;
+};
+shapeButton.onclick = () => {
+  shape = true;
+  console.log(shape);
 };
 function draw(context, x1, y1, x2, y2, color) {
   context.beginPath();
@@ -23,6 +36,14 @@ function draw(context, x1, y1, x2, y2, color) {
   context.stroke();
   context.closePath();
 }
+function drawRect(context, x, y, w, h, color) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.beginPath();
+  context.lineWidth = 10;
+  context.strokeStyle = color;
+  context.strokeRect(x, y, w, h);
+  context.closePath();
+}
 canvas.addEventListener("mousedown", (e) => {
   x = e.offsetX;
   y = e.offsetY;
@@ -31,17 +52,29 @@ canvas.addEventListener("mousedown", (e) => {
 
 canvas.addEventListener("mousemove", (e) => {
   if (Drawing === true) {
-    draw(ctx, x, y, e.offsetX, e.offsetY, color);
-    x = e.offsetX;
-    y = e.offsetY;
+    if (!shape) {
+      draw(ctx, x, y, e.offsetX, e.offsetY, color);
+      x = e.offsetX;
+      y = e.offsetY;
+    } else {
+      drawRect(ctx, x, y, e.offsetX - x, e.offsetY - y, color);
+    }
   }
 });
 
 window.addEventListener("mouseup", (e) => {
   if (Drawing === true) {
-    draw(ctx, x, y, e.offsetX, e.offsetY, color);
-    x = 0;
-    y = 0;
-    Drawing = false;
+    if (!shape) {
+      draw(ctx, x, y, e.offsetX, e.offsetY, color);
+      x = 0;
+      y = 0;
+      Drawing = false;
+    } else {
+      drawRect(ctx, x, y, e.offsetX - x, e.offsetY - y, color);
+      x = 0;
+      y = 0;
+      Drawing = false;
+      shape = false;
+    }
   }
 });
